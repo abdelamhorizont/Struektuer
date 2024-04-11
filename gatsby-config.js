c1bf188f6d98f8be507d45d297c00eb51c51aab5
abdelammeer <abdelhamid.ameur@hotmail.de>
2024-04-11 15:15:10 +0200

    first push

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,11 +6,16 @@ module.exports = {
     title: `struektuer-website`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: [{
-    resolve: 'gatsby-source-contentful',
-    options: {
-      "accessToken": "",
-      "spaceId": ""
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-sass"]
+  plugins: [
+  // {
+  //   resolve: 'gatsby-source-contentful',
+  //   options: {
+  //     "accessToken": "",
+  //     "spaceId": ""
+  //   }
+  // }, 
+  "gatsby-plugin-image", 
+  "gatsby-plugin-sharp", 
+  "gatsby-transformer-sharp", 
+  "gatsby-plugin-sass"]
 };

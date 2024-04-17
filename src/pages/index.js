7751c5ea986b8913e8a8bc0b28718e40f77c5f1a
abdelammeer <abdelhamid.ameur@hotmail.de>
2024-04-17 14:37:32 +0200

    c

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,79 @@
 import * as React from "react"
+import { useEffect, useState } from "react"
+import { motion, useMotionValue, useTransform, animate } from "framer-motion"
+
 import Logo from '../images/logo.svg'
-import rr from '../images/rr.svg'
+import icon1 from '../images/icon1.svg'
+import icon2 from '../images/icon2.svg'
+import icon3 from '../images/icon3.svg'
+import icon4 from '../images/icon4.svg'
+import icon5 from '../images/icon5.svg'
 
 // import './reset.scss'
 import './index.scss'
 
 const IndexPage = () => {
+  const count = useMotionValue(0)
+  const rounded = useTransform(count, latest => Math.round(latest))
+
+  const items = [icon1, icon2, icon3, icon4, icon5]
+  const [counter, setCounter] = useState(0)
+  // let counter = 0
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCounter(counter => counter + 1);
+    }, 1000);
+    
+    counter == items.length && setCounter(0)
+    
+    return () => {
+      clearInterval(interval);
+    };
+
+    // const controls = animate(count, items.length, { duration: 2 })
+    // return controls.stop
+  }, [counter])
+
+  const variants = {
+    visible: {
+      opacity: 1,
+      transition: {
+        ease: "linear",
+        duration: 0.1,
+        repeat: Infinity,
+        repeatDelay: 1,
+        repeatType: "mirror",
+      },
+    },
+    hidden: { opacity: 0 },
+  }
+
+
   return (
     <main>
       <header>
-        <img className="icon" src={rr} alt="Strüktür Icon" />
+        <div className="icons">
+          {/* <motion.img
+            initial="hidden"
+            animate="visible"
+            variants={variants}
+            id="icon1"
+            className="icon"
+            src={icon1}
+            alt="Strüktür Icon"
+          /> */}
+
+          {/* <motion.div
+            transition={{ duration: 5 }}
+          >
+            {rounded}</motion.div> */}
+          <motion.img id="icon4" className="icon" src={items[counter]} alt="Strüktür Icon" />
+          {/* <img id="icon3" className="icon" src={icon3} alt="Strüktür Icon" />
+          <img id="icon5" className="icon" src={icon5} alt="Strüktür Icon" />
+          <img id="icon2" className="icon" src={icon2} alt="Strüktür Icon" /> */}
+        </div>
+
         <img id="logo" src={Logo} alt="Strüktür Logo" />
       </header>
       <div className="content">
@@ -24,7 +88,7 @@ const IndexPage = () => {
           <a href="mailto:info@struektuer.org">
             info@struektuer.org
           </a> <br />
-     
+
           Instagram &nbsp;
           <a href="https://www.instagram.com/struektuer/">
             @struektuer
@@ -33,10 +97,10 @@ const IndexPage = () => {
         <p>
           Founded by <br />
           Bengisu Çağlayan <br />
-          Gözde Gazioğlu (<span>Chair, Board Member</span>)  <br /> 
+          Gözde Gazioğlu (<span>Chair, Board Member</span>)  <br />
           Yelta Köm <br />
-          Hatice Marx (<span>Board Member</span>) <br /> 
-          Tuna Ortaylı Kazıcı (<span>Treasurer, Board Member</span>) <br /> 
+          Hatice Marx (<span>Board Member</span>) <br />
+          Tuna Ortaylı Kazıcı (<span>Treasurer, Board Member</span>) <br />
           Vivien Tauchmann <br />
           Alper Turan <br />
           Prof. Dr. Füsun Türetken (<span>Chair, Board Member</span>)
